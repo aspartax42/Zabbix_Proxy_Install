b7245c1de3fd5f046b66972863a6f51f1fad356c
aspartax42 <59195908+aspartax42@users.noreply.github.com>
2020-09-30 10:55:39 +0200

    Update zabbix_proxy_install.sh

--- zabbix_proxy_install.sh
+++ zabbix_proxy_install.sh
@@ -8,14 +8,13 @@ fi
 # Principaux paramètres
 tput setaf 7; read -p "Entrer le mot de passe root de la base de données: " ROOT_DB_PASS
 tput setaf 7; read -p "Entrer le mot de passe zabbix de la base de données: " ZABBIX_DB_PASS
+tput setaf 7; read -p "Entre le nom du serveur principal Zabbix: " ZABBIX_SERVER
 tput setaf 7; read -p "Entrer le nom du proxy: " ZABBIX_PROXY_HOSTNAME
 tput setaf 7; read -p "Entrer le nom de l'indentité PSK: " PSK_ID
 
 tput setaf 2; echo ""
 
 # Récupération du paquet Zabbix 4.4
-#	rm zabbix-release_4.4-1+buster_all.*
-
 cd /tmp
 
 wget https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1%2Bbuster_all.deb
@@ -67,95 +66,72 @@ zcat /usr/share/doc/zabbix-proxy-mysql*/schema.sql.gz |  mysql -u zabbix --passw
 
 mysql -uroot -p'$ROOT_DB_PASS' -D zabbix_proxy -e "set global innodb_strict_mode='ON';"
 
-# Execution du script de modification du fichier /etc/zabbix/zabbix_proxy.conf
-echo "Server=monitoring.stodeo.com" > /etc/zabbix/zabbix_proxy.conf
-echo "Hostname="$ZABBIX_PROXY_HOSTNAME"" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "Conf Debut OK"
-
-echo "" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "LogFile=/var/log/zabbix/zabbix_proxy.log" >> /etc/zabbix/zabbix_proxy.conf
-echo "LogFileSize=1024" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "DBName=zabbix_proxy" >> /etc/zabbix/zabbix_proxy.conf
-echo "DBUser=zabbix" >> /etc/zabbix/zabbix_proxy.conf
-echo "DBPassword="$ZABBIX_DB_PASS >> /etc/zabbix/zabbix_proxy.conf
-
-echo "" >> /etc/zabbix/zabbix_proxy.conf
-echo "" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "#====== PROXY SPECIFIC PARAMETERS =======" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "ConfigFrequency=60" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "#====== ADVANCED PARAMETERS =======" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "StartPollers=10" >> /etc/zabbix/zabbix_proxy.conf
-echo "StartPollersUnreachable=2" >> /etc/zabbix/zabbix_proxy.conf
-echo "StartPingers=5" >> /etc/zabbix/zabbix_proxy.conf
-echo "StartDiscoverers=5" >> /etc/zabbix/zabbix_proxy.conf
-echo "CacheSize=128M" >> /etc/zabbix/zabbix_proxy.conf
-echo "HistoryIndexCacheSize=100M" >> /etc/zabbix/zabbix_proxy.conf
-echo "Timeout=30" >> /etc/zabbix/zabbix_proxy.conf
-echo "ExternalScripts=/usr/lib/zabbix/externalscripts" >> /etc/zabbix/zabbix_proxy.conf
-echo "FpingLocation=/usr/bin/fping" >> /etc/zabbix/zabbix_proxy.conf
-echo "Fping6Location=/usr/bin/fping6" >> /etc/zabbix/zabbix_proxy.conf
-echo "LogSlowQueries=3000" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "StartVMwareCollectors=5" >> /etc/zabbix/zabbix_proxy.conf
-echo "VMwareFrequency=60" >> /etc/zabbix/zabbix_proxy.conf
-echo "VMwareCacheSize=8M" >> /etc/zabbix/zabbix_proxy.conf
-echo "VMwareTimeout=10" >> /etc/zabbix/zabbix_proxy.conf
-
-
-echo "" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "#====== TLS-RELATED PARAMETERS =======" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "" >> /etc/zabbix/zabbix_proxy.conf
-
-echo "TLSConnect=psk" >> /etc/zabbix/zabbix_proxy.conf
-echo "TLSPSKFile=/etc/zabbix/zabbix_proxy.psk" >> /etc/zabbix/zabbix_proxy.conf
-echo "TLSPSKIdentity="$PSK_ID"" >> /etc/zabbix/zabbix_proxy.conf
-
-
-echo "Fin fichier conf"
-
-systemctl restart zabbix-proxy
-systemctl enable zabbix-proxy
-
 
 # Génération de la clé PSK
 openssl rand -hex 32 > /etc/zabbix/zabbix_proxy.psk
 chown zabbix:zabbix /etc/zabbix/zabbix_proxy.psk
 chmod 644 /etc/zabbix/zabbix_proxy.psk
 
+# Execution du script de modification du fichier /etc/zabbix/zabbix_proxy.conf
+echo "LogFile=/var/log/zabbix/zabbix_proxy.log
+LogFileSize=1024
+
+PidFile=/var/run/zabbix/zabbix_proxy.pid
+
+SocketDir=/var/run/zabbix
+
+DBName=zabbix_proxy
+DBUser=zabbix
+
+
+
+ConfigFrequency=60
+
+
+StartPollers=10
+StartPollersUnreachable=2
+StartPingers=5
+StartDiscoverers=5
+CacheSize=128M
+HistoryIndexCacheSize=100M
+Timeout=30
+ExternalScripts=/usr/lib/zabbix/externalscripts
+FpingLocation=/usr/bin/fping
+Fping6Location=/usr/bin/fping6
+LogSlowQueries=3000
+
+StartVMwareCollectors=5
+VMwareFrequency=60
+VMwareCacheSize=8M
+VMwareTimeout=10
+
+TLSConnect=psk
+TLSPSKFile=/etc/zabbix/zabbix_proxy.psk" > /etc/zabbix/zabbix_proxy.conf
+
+echo "Server="$ZABBIX_SERVER"" >> /etc/zabbix/zabbix_proxy.conf
+echo "TLSPSKIdentity="$PSK_ID"" >> /etc/zabbix/zabbix_proxy.conf
+echo "Hostname="$ZABBIX_PROXY_HOSTNAME"" >> /etc/zabbix/zabbix_proxy.conf
+echo "DBPassword="$ZABBIX_DB_PASS >> /etc/zabbix/zabbix_proxy.conf
+
+
 systemctl restart zabbix-proxy
+systemctl enable zabbix-proxy
 
-echo "PSK OK"
-
-
-echo "Zabbix actif"
+cd /tmp/
+rm zabbix-release_*
 
 clear
 echo "-------------------------------------------------"
-echo "       => Installation terminée <=       "
-echo ""
-echo "Voici le nom du proxy: "$ZABBIX_PROXY_HOSTNAME""
-echo ""
-echo "Voici la clé PSK généré automatiquement: "
-echo ""
-cat /etc/zabbix/zabbix_proxy.psk
+tput bold; tput setaf 7; echo "       => Installation terminée <=       "
+tput setaf 7; echo ""
+tput setaf 7; echo "Nom du proxy: "$ZABBIX_PROXY_HOSTNAME""
+tput setaf 7; echo ""
+tput setaf 7; echo "Nom du serveur principal Zabbix: "$ZABBIX_SERVER""
+tput setaf 7; echo ""
+tput setaf 7; echo "Voici la clé PSK généré automatiquement: "
+tput setaf 7; cat /etc/zabbix/zabbix_proxy.psk
 
-echo ""
-echo ""
-echo "       By Lilian COLLARD       "
+tput setaf 7; echo ""
+tput setaf 7; echo ""
+tput bold; tput setaf 6; echo "       By Lilian COLLARD       "
+tput setaf 2; echo ""
